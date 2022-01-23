b5e266662e89597efd3eb8cb2ecc24f11120f891
RaquelAmbroziof <ambrozioraquel@gmail.com>
2022-01-23 11:27:17 -0300

    salvando o score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dsmovie.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmovie.dto.MovieDto;
+import com.devsuperior.dsmovie.dto.ScoreDto;
+import com.devsuperior.dsmovie.services.ScoreService;
+
+@RestController
+@RequestMapping(value = "/scores")
+public class ScoreController {
+	
+	@Autowired
+	private ScoreService scoreService;
+	
+	
+	@PutMapping()
+	public MovieDto savescore(@RequestBody ScoreDto	scoreDto) {
+		MovieDto movieDto = scoreService.saveScore(scoreDto);
+		return movieDto;
+	}
+
+}
