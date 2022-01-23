c9cd025ecc665d40f5ca4a4251a3bea5ce3b12aa
RaquelAmbroziof <ambrozioraquel@gmail.com>
2022-01-22 21:03:58 -0300

    entities e conexao com banco de dados h2

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,47 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+	
+	/* usando o jpa a chave primaria tem que ser
+	apenas um atributo por isso criamos essa 
+	classe auxiliar ScorePk*/
+	
+	
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+	private Double value;
+	
+	public Score() {
+	}
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+}
